30ed66b47fc5f72640ab422510d53beeea0d2089
Charlie Meyer <cmeyer@homeaway.com>
2014-05-09 18:20:11 -0500

    enhance upnp discovery

--- lib/AVRControl/avr_context.rb
+++ lib/AVRControl/avr_context.rb
@@ -15,10 +15,20 @@ module AVRControl
         results = Playful::SSDP.search 'urn:schemas-upnp-org:device:MediaRenderer:1'
         receivers = []
         results.each do |renderer|
-          if renderer.has_key? :server and renderer[:server].include? 'KnOS'
-            receivers << AVRContext.new(URI.parse(renderer[:location]).host)
+          if renderer.has_key? :server and renderer[:server].include? 'KnOS' #they all run KnOS
+            host = URI.parse(renderer[:location]).host
+            Timeout::timeout(1) do #check if telnet is open
+              begin
+                port = 23
+                TCPSocket.new(host, port).close
+                receivers << AVRContext.new(host, port)
+              rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
+                next
+              end
+            end
           end
         end
+        sleep 0.5 #need this so that we dont run a command right away, as the receivers tend to crash if hit too often
         receivers
       end
     end
